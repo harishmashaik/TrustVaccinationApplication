0e1890e3121c675639c64b6f49c7e6527c13feed
harishmashaik <Harishmashaikk@gmail.com>
2021-07-31 01:00:45 -0400

    added reschedule appointment

---
+++ app/src/main/java/com/team4/getvaxi/RescheduleOrCancelActivity.java
@@ -0,0 +1,13 @@
+package com.team4.getvaxi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RescheduleOrCancelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reschedule_or_cancel);
+    }
+}
